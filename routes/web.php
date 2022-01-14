16460b0f18be88c8f583339a6cc05d558195aa22
chankaka <seba2110@gmail.com>
2022-01-14 15:56:25 -0300

    update 1.1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Main', function () {
+    return view('Main.index');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
